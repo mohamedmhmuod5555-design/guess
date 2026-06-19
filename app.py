be0088401189b431dc4cabf91b16ebd2c8360860
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-06-19 02:24:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import random
 number=random.randint(1,20)
 st.title("أهلا بك في لعبتنا التابعه للبروفييسير الاستاذ محمد احمد رياض ")
 name = st.text_input("من فضلك اعطيني اسمك")
-
-print("من فضلك ادخل تخمينك ")
-guess=int(input())
+guess=st.text_input("ادخل تخمينك ")
 if guess == number:
     print("you are winner")
 if guess > number:
     print("you are loser the number was ",number)  
 if guess < number:
     print("you are loser the number was ",number)       
+
