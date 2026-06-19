0648809976fb38384606b299984ff2d89c01a29c
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-06-19 02:26:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ st.title("أهلا بك في لعبتنا التابعه للبروفييسير 
 name = st.text_input("من فضلك اعطيني اسمك")
 guess=st.text_input("ادخل تخمينك ")
 if guess == number:
-    print("you are winner")
+    st.write("you are winner")
 if guess > number:
-    print("you are loser the number was ",number)  
+   st.write("you are loser the number was ",number)  
 if guess < number:
-    print("you are loser the number was ",number)       
-
+    st.write("you are loser the number was ",number)       
