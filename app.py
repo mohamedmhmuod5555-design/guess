d5c6f7bd66f93d8debdff62506abd5248e86d81f
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-06-19 02:13:35 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit  as st
+import random 
+number=random.randint(1,20)
+st.title("أهلا بك في لعبتنا التابعه للبروفييسير الاستاذ محمد احمد رياض ")
+st.write("من فضلك اعطيني اسمك ")
+st.write(" أهلا بك"+name)
+print("من فضلك ادخل تخمينك ")
+guess=int(input())
+if guess == number:
+    print("you are winner")
+if guess > number:
+    print("you are loser the number was ",number)  
+if guess < number:
+    print("you are loser the number was ",number)       
