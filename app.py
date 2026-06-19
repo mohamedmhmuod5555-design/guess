cb66c6cc56cf484d9ee71e0dfff25f8d465848fb
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-06-19 02:16:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit  as st
 import random 
 number=random.randint(1,20)
 st.title("أهلا بك في لعبتنا التابعه للبروفييسير الاستاذ محمد احمد رياض ")
-st.write("من فضلك اعطيني اسمك ")
+print(" ما اسمك ")
+name=input()
 st.write(" أهلا بك"+name)
 print("من فضلك ادخل تخمينك ")
 guess=int(input())
